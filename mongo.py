a426582b6f0a87add71db9d71231b69b802b13be
techexpert1611 <techexpert1611@gmail.com>
2020-12-25 18:00:43 +0530

    fix: Update code for column object.

--- mongo.py
+++ mongo.py
@@ -2,9 +2,9 @@ import pymongo
 
 mongo_client = pymongo.MongoClient('mongodb://localhost:27017/')
 database = mongo_client['test_db']
+appointment_col = database["patient_appointment"]
 
 
 def insert_appointment(appointment_dict):
-    appointment_col = database["patient_appointment"]
     appointment_col.insert_one(appointment_dict)
     print("Appointment inserted!")
